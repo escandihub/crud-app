f90a7955e5897de0ee3a915875d84b9443046357
Escandon <aleskndon@hotmail.com>
2021-02-09 12:19:35 -0600

    feat: manejo de login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+	public function login(Request $request)
+	{
+		$request->validate([
+			"email" => "required|email",
+			"password" => "required",
+		]);
+		$user = User::where("email", $request->email)->first();
+		if (!$user || !Hash::check($request->password, $user->password)) {
+			throw ValidationException::withMessages([
+				"email" => ["Las credenciales son incorrectas."],
+			]);
+		}
+
+		return $user->createToken($user->name)->plainTextToken;
+	}
+
+	public function logout(Request $request)
+	{
+		return $request->user()->tokens()->delete();
+	}
+}

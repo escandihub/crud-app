2095220d9ea458c582ce330e40501b0b042f2675
Escandon <aleskndon@hotmail.com>
2021-02-09 13:07:20 -0600

    feat: restriccion de acceso estas restricciones son para los metodos de crear y update

--- app/Http/Controllers/PersonalController.php
+++ app/Http/Controllers/PersonalController.php
@@ -4,42 +4,67 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\Product; //es un producto 
+use App\Models\Product; //es un producto
 use App\Http\Requests\PersonalRequest;
 
 class PersonalController extends Controller
 {
-    public function index(Type $var = null)
-    {
-        return response()->json(Product::all(), 200);
-    }
+	public function index(Type $var = null)
+	{
+		return response()->json(Product::all(), 200);
+	}
 
-    public function store(PersonalRequest $request)
-    {
-        // \Gate::authorize('hasRole', "alumno");
-        
-        Product::create($request->all());
-        return response()->json([
-            'message' => "se ha guardado con exito."
-        ], 200);
-    }
+	public function store(PersonalRequest $request)
+	{
+		Product::create($request->all() + ["user_id" => \Auth::user()->id]);
+		return response()->json(
+			[
+				"message" => "se ha guardado con exito.",
+			],
+			200
+		);
+	}
 
-    public function show(Product $personal)
-    {
-        return response()->json($personal, 200);
-    }
-    public function update(PersonalRequest $request, Product $personal)
-    {
-        $personal->update($request->all());
-        return response()->json([
-            'message' => 'Se ha actualizado'
-        ], 200);
-    }
-    public function destroy(Product $personal)
-    {
-        $personal->delete();
-        return response()->json([
-            'message' => 'Se ha eliminado, con exito.'
-        ], 200);
-    }
+	public function show(Product $personal)
+	{
+		return response()->json($personal, 200);
+	}
+
+	/**
+	 * para actualizar, solo el administrador prodra realizarlo
+	 * o si quien lo creo es el usuario autentificado
+	 *
+	 * @param PersonalRequest $request
+	 * @param Product $personal
+	 * @return void
+	 */
+	public function update(PersonalRequest $request, Product $personal)
+	{
+		$this->authorize("update", $personal);
+		$personal->update($request->all());
+		return response()->json(
+			[
+				"message" => "Se ha actualizado",
+			],
+			200
+		);
+	}
+	/**
+	 * a este metodo solo el administrador
+	 * puede eliminimar
+	 *
+	 * @param Product $personal
+	 * @return void
+	 */
+	public function destroy(Product $personal)
+	{
+		\Gate::authorize("hasRole", "admin"); //AuthorizationException | es lo que retorna si es falso
+		$personal->delete();
+		return response()->json(
+			[
+				"message" => "Se ha eliminado, con exito.",
+			],
+			200
+		);
+	}
 }

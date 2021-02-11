088ad20081cdd80fdc3a71b2b9ce335eec289dc0
Escandon <aleskndon@hotmail.com>
2021-02-11 11:40:30 -0600

    add: provider event listener for github

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -21,6 +21,7 @@ class EventServiceProvider extends ServiceProvider
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
         // ... other providers
         'SocialiteProviders\\Facebook\\FacebookExtendSocialite@handle',
+        'SocialiteProviders\\GitHub\\GitHubExtendSocialite@handle', //es util
     ],
     ];
 

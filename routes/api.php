a56806b083d34633c02417861b2eaa14de8d786a
Escandon <aleskndon@hotmail.com>
2021-02-14 11:56:17 -0600

    upt: move route to webhook.php

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonalController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LoginProvidersController;
-use App\Http\Controllers\SendWebhookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,6 +40,3 @@ Route::get('test', function () {
 //login 3er party
 Route::get("login/{provider}", [LoginProvidersController::class, "redirectToProvider"]);
 Route::get("login/{provider}/callback", [LoginProvidersController::class, "handleProviderCallback"]);
-
-// route to invoque the webhook
-Route::get('send-webhook', [SendWebhookController::class, 'send'])->name('send-webhook');

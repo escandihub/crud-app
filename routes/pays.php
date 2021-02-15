0cd0e5c2b2d07a5d4480dadd5e77c91644bbe9ce
Escandon <aleskndon@hotmail.com>
2021-02-15 15:24:21 -0600

    upt: rutes update

--- routes/pays.php
+++ routes/pays.php
@@ -3,8 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\GeneratePayController;
+use App\Http\Controllers\Openpay\Customer;
+use App\Http\Controllers\Openpay\BankAccountController;
 
 Route::prefix("pays")->group(function () {
 	Route::any("link-generator", [GeneratePayController::class, "basicPay"]);
 	Route::get("cargos", [GeneratePayController::class, "getCharges"]);
+	Route::get("pagar", [GeneratePayController::class, "payCharge"]);
+
+	//create an client
+	Route::get("makeCustomer", [Customer::class, "createCustomer"]);
+	Route::get("cuenta-cliente", [Customer::class, "customerAccount"]);
+	//account administration
+	Route::get("card", [BankAccountController::class, "createCard"]);
 });

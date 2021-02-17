959248144ee65f744f18f9495d11ce6b796a6e8a
Escandon <aleskndon@hotmail.com>
2021-02-17 09:47:45 -0600

    prev: webhook cotroller & route

---
+++ app/Http/Controllers/Gupshup/WebhookController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Gupshup;
+
+use Illuminate\Http\Request;
+
+class WebhookController extends Controller
+{
+    public function webhook()
+    {
+        
+    }
+}
--- routes/bot.php
+++ routes/bot.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Gupshup\SendController;
+use App\Http\Controllers\Gupshup\WebhookController;
 
 Route::prefix("bot")->group(function () {
 	Route::get("send", [SendController::class, "send"]);
+	Route::get("webhook", [WebhookController::class, "webhook"]);
+
 });

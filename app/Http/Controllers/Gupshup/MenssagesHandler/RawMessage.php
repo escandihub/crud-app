dd340cdc41be47d6e1a6d584a354740c84498eca
Escandon <aleskndon@hotmail.com>
2021-02-19 16:33:37 -0600

    upt: class reply to user

--- app/Http/Controllers/Gupshup/MenssagesHandler/RawMessage.php
+++ app/Http/Controllers/Gupshup/MenssagesHandler/RawMessage.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers\Gupshup\MenssagesHandler;
 
 use Illuminate\Support\Collection;
-
+use App\Http\Controllers\Gupshup\Connection;
 class RawMessage
 {
-    private $message;
-    public function __construct($message = []) {
-        $this->message = $message;
-    }
-    public function playground()
-    {
-        \Log::alert($this->message);
-    }
+	private $message;
+	public function __construct($message = [])
+	{
+		$this->message = $message;
+	}
+	public function playground()
+	{
+		\Log::info($this->message);
+		$col = collect($this->message);
+
+		    $dial = $col['payload']['sender']['dial_code'];
+		    // \Log::info(gettype($dial));
+		    $msg = new Connection();
+		    $msg->replyTo($dial, 'hola perdido');
+	}
 }

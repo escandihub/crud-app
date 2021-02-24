f781b1037b6956a68277a0b5e9b015a08cdaebb6
Escandon <aleskndon@hotmail.com>
2021-02-23 20:45:12 -0600

    upt: use a final class to create a connection

--- app/Http/Controllers/Openpay/OpenpayHelper.php
+++ app/Http/Controllers/Openpay/OpenpayHelper.php
@@ -4,30 +4,19 @@ namespace App\Http\Controllers\Openpay;
 
 use Openpay;
 
-class OpenpayHelper
+final class OpenpayHelper
 {
-	public static $openPay;
-	public function __construct()
+	private static $openPay = null;
+
+	public static function getInstance()
 	{
-		/**
-		 *  CLIENT ID, PRIVATE KEY , COUNTRY CODE
-		 */
-		self::$openPay = Openpay::getInstance(
+		if (static::$openPay === null) {
+			static::$openPay = new static();
+		}
+		return Openpay::getInstance(
 			env("OPENPAY_MERCHANT_ID"),
 			env("OPENPAY_PRIVATE_KEY"),
 			env("OPENPAY_COUNTRY_CODE")
 		);
-
-		Openpay::setProductionMode(false);
-	}
-
-	/**
-	 * instance of Openpay
-	 *
-	 * @return Openpay Object
-	 */
-	public static function getInstance()
-	{
-		return self::$openPay;
 	}
 }

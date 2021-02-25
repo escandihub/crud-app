a9c40c5645d22a944bd1f805b2eab82f1e40e68c
Escandon <aleskndon@hotmail.com>
2021-02-25 15:05:41 -0600

    upt: filter if type of incomming messge check if message is expected

--- app/Http/Controllers/Gupshup/InboundMessage/Message.php
+++ app/Http/Controllers/Gupshup/InboundMessage/Message.php
@@ -6,6 +6,12 @@ class Message implements iMessages
 {
 	public function process($message)
 	{
-		echo 'controlar la respuesta del mensaje';
+		echo 'controlar la respuesta del mensaje, mi total *$20*, a _pagar_ ~better~ best, ```hello word```';
+	}
+
+	public function expectedType(string $type): bool
+	{
+		if(!in_array($type, ['text', 'file', 'photo']))
+		return 'Este dato no se esta esperando, verifique su respuesta';
 	}
 }

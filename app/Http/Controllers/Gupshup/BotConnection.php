d8fa797b462b7638dff354d76e861ccfcacaecf4
Escandon <aleskndon@hotmail.com>
2021-02-25 16:23:47 -0600

    upt: methods to list opt-ins

--- app/Http/Controllers/Gupshup/BotConnection.php
+++ app/Http/Controllers/Gupshup/BotConnection.php
@@ -5,12 +5,13 @@ use Illuminate\Support\Facades\Http;
 final class BotConnection
 {
 	const API = "https://api.gupshup.io/sm/api/v1/msg";
+	const API_OPT_IN = "https://api.gupshup.io/sm/api/v1/app/opt/in/";
+	const API_USERS = "https://api.gupshup.io/sm/api/v1/users/";
 	private static $client = null;
 
 	public static function sendTo($number)
 	{
 		return Http::withHeaders([
-			"Cache-Control" => "no-cache",
 			"apikey" => env("GUPSHUP_API_KEY"),
 		])
 			->asForm()
@@ -21,5 +22,23 @@ final class BotConnection
 				"message" => "¿te gusta?",
 				"src.name" => env("BOT_NAME"),
 			]);
+	}
+
+	public static function requestOptIn($number)
+	{
+		return Http::withHeaders([
+			"apikey" => env("GUPSHUP_API_KEY"),
+		])
+			->asForm()
+			->post(self::API_OPT_IN . env("BOT_NAME"), [
+				"user" => $number,
+			]);
+	}
+
+	public static function getUsers()
+	{
+		return Http::withHeaders([
+			"apikey" => env("GUPSHUP_API_KEY"),
+		])->get(self::API_USERS . env("BOT_NAME"));
 	}
 }

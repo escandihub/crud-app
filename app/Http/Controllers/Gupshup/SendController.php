92514b3f36f5a49a5c47ef3dd8bc3adc1875c755
Escandon <aleskndon@hotmail.com>
2021-02-25 13:42:41 -0600

    upt: use singleton conecc

--- app/Http/Controllers/Gupshup/SendController.php
+++ app/Http/Controllers/Gupshup/SendController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Gupshup;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use  App\Http\Controllers\Gupshup\Connection;
+use  App\Http\Controllers\Gupshup\BotConnection;
 class SendController extends Controller
 {
     public $conec;
     public function send()
     {
-        $conec = new Connection;
-      dd($conec->authentication());
+        // $conec = new Connection;
+      // dd($conec->authentication());
+      dd(BotConnection::sendTo("19612954393"));
+      \Log::alert('todo bien');
     }
 }

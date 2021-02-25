9973124694024284127e2d2727c15750aeef9590
Escandon <aleskndon@hotmail.com>
2021-02-25 13:44:06 -0600

    upt: handle messages fer event

--- app/Http/Controllers/Gupshup/MenssagesHandler/RawMessage.php
+++ app/Http/Controllers/Gupshup/MenssagesHandler/RawMessage.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Gupshup\MenssagesHandler;
 
 use Illuminate\Support\Collection;
 use App\Http\Controllers\Gupshup\Connection;
+use App\Http\Controllers\Gupshup\InboundMessage\InboundMessageFactory;
 class RawMessage
 {
 	private $message;
@@ -13,12 +14,17 @@ class RawMessage
 	}
 	public function playground()
 	{
-		\Log::info($this->message);
-		$col = collect($this->message);
-
-		    $dial = $col['payload']['sender']['dial_code'];
-		    // \Log::info(gettype($dial));
-		    $msg = new Connection();
-		    $msg->replyTo($dial, 'hola perdido');
+		\Log::error($this->message['type']); //as array
+		// $obj = json_decode($this->message, true);
+		// \Log::info($obj->type);
+		$mensajeEntrada = new InboundMessageFactory();
+		$type = $mensajeEntrada->initializableMessage($this->message["type"]);
+		$type->process($this->message);
+		// $col = collect($this->message); // as collection
+		// \Log::info($col->first());
+		//     $dial = $col['payload']['sender']['dial_code'];
+		//     // \Log::info(gettype($dial));
+		//     $msg = new Connection();
+		//     $msg->replyTo($dial, 'hola perdido');
 	}
 }

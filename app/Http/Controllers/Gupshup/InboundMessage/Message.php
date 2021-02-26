d767e72b1af10620bad6c1020cde410e3999c0a9
Escandon <aleskndon@hotmail.com>
2021-02-26 13:32:06 -0600

    upt: reponse with excusive command

--- app/Http/Controllers/Gupshup/InboundMessage/Message.php
+++ app/Http/Controllers/Gupshup/InboundMessage/Message.php
@@ -1,17 +1,26 @@
 <?php
 namespace App\Http\Controllers\Gupshup\InboundMessage;
 use App\Http\Controllers\Gupshup\InboundMessage\iMessages;
-
+use App\Http\Controllers\Gupshup\InboundMessage\TyposMessage\ReplyToTypos;
 class Message implements iMessages
 {
 	public function process($message)
 	{
-		echo 'controlar la respuesta del mensaje, mi total *$20*, a _pagar_ ~better~ best, ```hello word```';
+		// echo 'controlar la respuesta del mensaje, mi total *$20*, a _pagar_ ~better~ best, ```hello word```';
+		// \Log::alert($message);
+		$msg = $message["payload"]["payload"]["text"];
+
+		if (in_array($msg, ["imagen", "video", "audio"])) {
+			$typos = new ReplyToTypos();
+			call_user_func([$typos, $msg]);
+		}
+		echo "no contamos con ese comando";
 	}
 
 	public function expectedType(string $type): bool
 	{
-		if(!in_array($type, ['text', 'file', 'photo']))
-		return 'Este dato no se esta esperando, verifique su respuesta';
+		if (!in_array($type, ["text", "file", "photo"])) {
+			return "Este dato no se esta esperando, verifique su respuesta";
+		}
 	}
 }

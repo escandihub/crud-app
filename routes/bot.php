c435dbf2daaadcd0fb3c431dba753d02f40de915
Escandon <aleskndon@hotmail.com>
2021-03-03 21:22:27 -0600

    upt: endpoint testing testing to send

--- routes/bot.php
+++ routes/bot.php
@@ -4,9 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Gupshup\SendController;
 use App\Http\Controllers\Gupshup\WebhookController;
+use App\Http\Controllers\Gupshup\MenssagesHandler\SenderFactoryController;
 
 Route::prefix("bot")->group(function () {
 	Route::get("send", [SendController::class, "send"]);
 	Route::post("webhook", [WebhookController::class, "webhook"]);
+	Route::get("req-opt", [WebhookController::class, "webhook"]);
 
+	Route::get("send-img", [SenderFactoryController::class, "sendImagen"]);
+	Route::get("send-audio", [SenderFactoryController::class, "sendAudio"]);
+	Route::get("send-video", [SenderFactoryController::class, "sendVideo"]);
+	Route::get("send-location", [SenderFactoryController::class, "sendLocation"]);
+	Route::get("send-stiker", [SenderFactoryController::class, "sendStiker"]);
 });
